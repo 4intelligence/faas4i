bb4fde6b2213102d0d832cd2d4f45133188b0e86
natalia-araujo <69853529+natalia-araujo@users.noreply.github.com>
2022-05-11 13:31:37 -0300

    Release - v0.2.2 (#37)

--- R/package_version_check.R
+++ R/package_version_check.R
@@ -13,29 +13,35 @@
 #' @rdname package_version_check
 #' @seealso
 #'  \code{\link[remotes]{install_github}}
+#' @importFrom httr GET content
 #' @importFrom remotes install_github
 package_version_check <- function(repo = "https://github.com/4intelligence/faas4i") {
 
   ## Getting latest tag and making it into a package version object
-  latest_tag <- system(paste0("git ls-remote --refs --tags ", repo, " | cut --delimiter='/' --fields=3 | tr '-' '~'| sort --version-sort | tail --lines=1"), intern = T)
+  uri <- 'https://api.github.com/repos/4intelligence/faas4i/releases/latest'
+  git_response <- httr::GET(uri)
+  latest_tag <- httr::content(git_response)[["tag_name"]]
   latest_tag <- gsub("^v", "", latest_tag)
   latest_tag <- as.package_version(latest_tag)
-  ## If can't find latest tag, we return nothing
-  if(length(latest_tag) == 0) {return(invisible())}
 
   ## Obtaining installed version of faas4i
   package_version <- packageVersion("faas4i")
-
-  if (latest_tag > package_version){
+  
+  if (latest_tag != package_version){
     var1 <- readline(prompt =cat("'faas4i' has a more recent version, would you like to update it now? (y/n)
     y: YES (recommended)
     n: NO "))
 
-    if (var1 == "y"){
+    if (tolower(var1) == "y"){
       remotes::install_github("4intelligence/faas4i", force = TRUE)
       warning("Please restart your R session.")
       return(TRUE)
+    } else{
+      if (tolower(var1) != "n"){
+        stop('Invalid input, please enter Y/y for yes or N/n for No.')
+      }
     }
   }
+
   return(FALSE)
 }
